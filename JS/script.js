df9117c41addf4bde7a1b3fdf7f7bbd99d66cb87
MarcoBione <marcobione.work@gmail.com>
2023-03-30 18:36:30 +0200

    Add some style @ style + @ index + @ script

--- JS/script.js
+++ JS/script.js
@@ -5,22 +5,7 @@ createApp({
         return {
 
             toDoList: [
-                {
-                    text: 'item1',
-                    done: false
-                },
-                {
-                    text: 'item2',
-                    done: false
-                },
-                {
-                    text: 'item3',
-                    done: false
-                },
-                {
-                    text: 'item4',
-                    done: false
-                },
+                
             ]
         }
     },
